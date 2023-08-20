e3b175443965b7854883d06747e96fc363d154fb
LMO GerritBot <gerrit@libremobileos.com>
2023-08-20 11:52:25 +0000

    Adapt to LMODroid by device-add script

--- common.mk
+++ common.mk
@@ -34,7 +34,7 @@ PRODUCT_COMPRESSED_APEX := false
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lmodroid
 
 # Audio
 PRODUCT_PACKAGES += \
